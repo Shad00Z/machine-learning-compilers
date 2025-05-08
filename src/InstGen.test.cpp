51ebed28d8af51d651b71c617d2c4fbf8146a7aa
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2025-05-08 19:21:04 +0200

    STR & STP tests

--- src/InstGen.test.cpp
+++ src/InstGen.test.cpp
@@ -88,3 +88,46 @@ TEST_CASE("Tests the Neon LDP instruction generation", "[Neon LDP]")
     l_hex = l_gen.to_string_hex(l_ins);
     REQUIRE(l_hex == "0x6dc10801");
 }
+
+TEST_CASE("Tests the Base STR instruction generation", "[Base STR]")
+{
+    mini_jit::InstGen l_gen;
+
+    uint32_t l_ins = mini_jit::InstGen::base_str_imm_uoff(mini_jit::InstGen::x1, mini_jit::InstGen::x0, 16);
+    std::string l_hex = l_gen.to_string_hex(l_ins);
+    REQUIRE(l_hex == "0xf9000801");
+}
+
+TEST_CASE("Tests the Base STP instruction generation", "[Base STP]")
+{
+    mini_jit::InstGen l_gen;
+
+    uint32_t l_ins = mini_jit::InstGen::base_stp_soff(mini_jit::InstGen::x2, mini_jit::InstGen::x3, mini_jit::InstGen::x0, -16);
+    std::string l_hex = l_gen.to_string_hex(l_ins);
+    REQUIRE(l_hex == "0xa93f0c02");
+
+    l_ins = mini_jit::InstGen::base_stp_post(mini_jit::InstGen::x2, mini_jit::InstGen::x3, mini_jit::InstGen::x0, 16);
+    l_hex = l_gen.to_string_hex(l_ins);
+    REQUIRE(l_hex == "0xa8810c02");
+
+    l_ins = mini_jit::InstGen::base_stp_pre(mini_jit::InstGen::x2, mini_jit::InstGen::x3, mini_jit::InstGen::x0, 16);
+    l_hex = l_gen.to_string_hex(l_ins);
+    REQUIRE(l_hex == "0xa9810c02");
+}
+
+TEST_CASE("Tests the Neon STP instruction generation", "[Neon STP]")
+{
+    mini_jit::InstGen l_gen;
+
+    uint32_t l_ins = mini_jit::InstGen::neon_stp_soff(mini_jit::InstGen::v1, mini_jit::InstGen::v2, mini_jit::InstGen::x0, -16, mini_jit::InstGen::d);
+    std::string l_hex = l_gen.to_string_hex(l_ins);
+    REQUIRE(l_hex == "0x6d3f0801");
+
+    l_ins = mini_jit::InstGen::neon_stp_post(mini_jit::InstGen::v1, mini_jit::InstGen::v2, mini_jit::InstGen::x0, 16, mini_jit::InstGen::d);
+    l_hex = l_gen.to_string_hex(l_ins);
+    REQUIRE(l_hex == "0x6c810801");
+
+    l_ins = mini_jit::InstGen::neon_stp_pre(mini_jit::InstGen::v1, mini_jit::InstGen::v2, mini_jit::InstGen::x0, 16, mini_jit::InstGen::d);
+    l_hex = l_gen.to_string_hex(l_ins);
+    REQUIRE(l_hex == "0x6d810801");
+}
