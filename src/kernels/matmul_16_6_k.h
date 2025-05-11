cd65ad9ed3c6768b475555a24bffcc2dea25b853
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2025-05-11 22:37:56 +0200

    prepare code

--- src/kernels/matmul_16_6_k.h
+++ src/kernels/matmul_16_6_k.h
@@ -10,8 +10,10 @@ namespace mini_jit
         /**
          * @brief Kernel for batch-reduce matrix multiplication.
          * @param kernel Kernel object to be filled with instructions.
+         * @param k number of columns in A and rows in B.
          */
-        void matmul_16_6_k( mini_jit::Kernel &kernel, int k );      
+        void matmul_16_6_k( mini_jit::Kernel &kernel, 
+                            int k );      
     }
 };
 
---
+++ src/kernels/matmul_m_6_k.h
@@ -0,0 +1,35 @@
+#ifndef MINI_JIT_MATMUL_M_6_K_H
+#define MINI_JIT_MATMUL_M_6_K_H
+
+#include "Kernel.h"
+
+namespace mini_jit
+{
+    namespace kernels
+    {
+        namespace internal
+        {
+            /**
+             * @brief Generates on M loop for matrix multiplication.
+             * @param kernel Kernel object to be filled with instructions.
+             * @param m number of rows in A and C.
+             * @param k number of columns in A and rows in B.
+             */
+            void generateMLoop( mini_jit::Kernel &kernel, 
+                                int m, 
+                                int k );
+        }
+
+        /**
+         * @brief Kernel for batch-reduce matrix multiplication.
+         * @param kernel Kernel object to be filled with instructions.
+         * @param m number of rows in A and C.
+         * @param k number of columns in A and rows in B.
+         */
+        void matmul_m_6_k( mini_jit::Kernel &kernel, 
+                           int m, 
+                           int k );      
+    }
+};
+
+#endif
