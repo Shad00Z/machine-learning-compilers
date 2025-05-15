ba7ef073ae6785041c594fe24b45c92ae2f1201d
xLPMG <xLPMG@users.noreply.github.com>
2025-05-15 16:21:49 +0000

    Deploying to gh-pages from @ Shad00Z/machine-learning-compilers@7c80fb6d9fb91342988b1321779abd1ad4fab5ba 🚀

--- search/functions_1.js
+++ search/functions_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['generate_0',['generate',['../classmini__jit_1_1Brgemm.html#a491982ce1ba45de53d7847cc1b6dc100',1,'mini_jit::Brgemm']]],
   ['get_5fkernel_1',['get_kernel',['../classmini__jit_1_1Brgemm.html#a8bb87d46eb0d84fca3241d8f1c69662a',1,'mini_jit::Brgemm::get_kernel()'],['../classmini__jit_1_1Kernel.html#a003e9eb9eabb03749e8700fdc45f6797',1,'mini_jit::Kernel::get_kernel() const']]],
-  ['get_5fsize_2',['get_size',['../classmini__jit_1_1Kernel.html#ae231a8fa325231b6d7f0c3b1c345250f',1,'mini_jit::Kernel']]]
+  ['get_5fsize_2',['get_size',['../classmini__jit_1_1Kernel.html#ae231a8fa325231b6d7f0c3b1c345250f',1,'mini_jit::Kernel']]],
+  ['getinstrcountfromlabel_3',['getInstrCountFromLabel',['../classmini__jit_1_1Kernel.html#a2845923d13df854cd3a4e19795257a34',1,'mini_jit::Kernel']]]
 ];
