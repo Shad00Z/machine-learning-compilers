274ac4f4273959f26ad7bd1d8e805f820cd178e9
AsZen <92991272+Shad00Z@users.noreply.github.com>
2025-06-11 23:35:21 +0200

    feat: einsum tree setup

---
+++ src/einsum/EinsumNode.h
@@ -0,0 +1,55 @@
+#ifndef MINI_JIT_EINSUM_EINSUM_NODE_H
+#define MINI_JIT_EINSUM_EINSUM_NODE_H
+
+#include <vector>
+
+#include "Dimension.h"
+#include "TensorOperation.h"
+
+namespace mini_jit
+{
+    namespace einsum
+    {
+        struct EinsumNode
+        {
+            // The node is the output for a contraction / permutation
+            std::vector<mini_jit::ir::Dimension> dimensions;
+
+            // The node is an input for a contraction / permutation
+            std::vector<mini_jit::ir::Dimension> dimensions_in;
+
+            std::vector<int64_t> dimension_ids;
+
+            EinsumNode *leftChild, *rightChild;
+
+            mini_jit::TensorOperation operation;
+
+            /**
+             *
+             */
+            EinsumNode(std::vector<int64_t> const &dimension_ids,
+                       EinsumNode *left,
+                       EinsumNode *right)
+                : dimension_ids(dimension_ids), leftChild(left), rightChild(right)
+            {
+            }
+
+            int64_t get_number_of_children()
+            {
+                int64_t result = 0;
+
+                if (leftChild != nullptr)
+                {
+                    result++;
+                }
+                if (rightChild != nullptr)
+                {
+                    result++;
+                }
+
+                return result;
+            }
+        };
+    }
+}
+#endif // MINI_JIT_EINSUM_EINSUM_NODE_H
