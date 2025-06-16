9230f4ee9e2f66975b2f43c8d2e62b1253fe1a05
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2025-06-16 19:26:00 +0200

    change docs

--- src/einsum/EinsumTree.h
+++ src/einsum/EinsumTree.h
@@ -24,7 +24,7 @@ class mini_jit::einsum::EinsumTree
 {
 public:
     /**
-     * @brief Helper function to parse the einsum expression and create nodes.
+     * @brief Helper function to parse the einsum expression and create an Einsum Tree.
      *
      * @param einsum_expression The string representation of the einsum operation.
      * @param dimension_sizes A vector to store the sizes of the dimensions used in the expression.
@@ -34,7 +34,7 @@ public:
                                                std::vector<int64_t> &dimension_sizes);
 
     /**
-     * @brief Execute the einsum tree.
+     * @brief Executes the einsum tree using tensor operations.
      *
      * @param root_node The root EinsumNode of the einsum tree.
      * @param dimension_sizes A vector containing the sizes of the dimensions used in the expression.
@@ -46,7 +46,7 @@ public:
 
 private:
     /**
-     * @brief Helper function to parse the einsum expression and create nodes recursively.
+     * @brief Helper function to parse the einsum expression and create an Einsum Tree recursively.
      *
      * @param einsum_expression The string representation of the einsum operation.
      * @return A EinsumNode representing the parsed expression.
@@ -62,9 +62,10 @@ private:
     static std::vector<int64_t> get_dimensions_from_expression(std::string const &einsum_expression);
 
     /**
-     * @brief Helper function to parse the einsum expression and create nodes recursively.
+     * @brief Helper function to initialize the dimensions, sizes and strides of the given
+     * einsum node and all of its children.
      *
-     * @param einsum_node
+     * @param einsum_node The root node of the einsum tree.
      * @param dimension_sizes The array with the dimension sizes sorted by id.
      * @return A EinsumNode representing the parsed expression.
      */
@@ -89,19 +90,20 @@ public:
     //! Deleted constructor to prevent instantiation of the static EinsumTree class.
     EinsumTree() = delete;
 
-     /**
-     * @brief Optimize the einsum nodes by applying transformations to the execution types and dimensions.
+    /**
+     * @brief Optimize the given einsum node and its children by applying transformations to the
+     * dimensions, sizes and strides.
      *
      * @param einsum_node The root node of the einsum tree.
      * @param thread_target The target number of threads for parallel execution.
      * @param max_kernel_size The maximum size of the kernel to be used.
      */
     static void optimize_einsum_nodes(EinsumNode *einsum_node,
-                                       int64_t thread_target,
-                                       int64_t max_kernel_size);
+                                      int64_t thread_target,
+                                      int64_t max_kernel_size);
 
     /**
-     * @brief Lower the einsum nodes to tensor operations by setting up the operation and dimensions.
+     * @brief Lower the given einsum node and all of its children to executable tensor operations.
      *
      * @param einsum_node The root node of the einsum tree.
      * @param dimension_sizes The array with the dimension sizes sorted by id.
