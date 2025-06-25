7b2b7fc9ea9d9e844fc034d0954d572028afca75
xLPMG <xxlpmghdxx@gmail.com>
2025-06-25 17:35:43 +0000

    Deploying to gh-pages from @ Shad00Z/machine-learning-compilers@88ed1e601c8497d2d8f3239514fba046427eca0f 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['benchmark_0',['Benchmark',['../classmini__jit_1_1Benchmark.html',1,'mini_jit']]],
   ['benchmark_5fresult_1',['benchmark_result',['../structmini__jit_1_1Benchmark_1_1benchmark__result.html',1,'mini_jit::Benchmark']]],
-  ['brgemm_2',['Brgemm',['../classmini__jit_1_1Brgemm.html',1,'mini_jit']]]
+  ['binary_2',['Binary',['../classmini__jit_1_1Binary.html',1,'mini_jit']]],
+  ['brgemm_3',['Brgemm',['../classmini__jit_1_1Brgemm.html',1,'mini_jit']]]
 ];
