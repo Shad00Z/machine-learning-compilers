4002250256c26f254534c2946e337732b18b457f
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2025-07-01 14:05:52 +0200

    move new benchmarks to correct folder

--- src/benchmarks/all_benchmarks.h
+++ src/benchmarks/all_benchmarks.h
@@ -4,17 +4,16 @@
 #include "matmul/Matmul_br_m_n_k.bench.h"
 #include "matmul/Matmul_m_n_k.bench.h"
 
+#include "unary/fast_sigmoid_primitive.bench.h"
 #include "unary/identity_primitive.bench.h"
 #include "unary/identity_trans_primitive.bench.h"
+#include "unary/reciprocal_primitive.bench.h"
 #include "unary/relu_primitive.bench.h"
 #include "unary/relu_trans_primitive.bench.h"
 #include "unary/square_primitive.bench.h"
 #include "unary/square_trans_primitive.bench.h"
 #include "unary/zero_eor_primitive.bench.h"
 #include "unary/zero_xzr_primitive.bench.h"
-
-#include "binary/reciprocal_primitive.bench.h"
-#include "binary/fast_sigmoid_primitive.bench.h"
 
 #include "EinsumTree.bench.h"
 #include "TensorOperation.bench.h"
